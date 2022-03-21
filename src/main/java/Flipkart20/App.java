0fae4f034a9bf78a50e6f36b111993a3fd332c90
Rushi <rushikesh@company.com>
2022-03-21 09:36:43 +0530

    dev has done for ac servicses flipkart20

---
+++ src/main/java/Flipkart20/App.java
@@ -0,0 +1,13 @@
+package Flipkart20;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Rushi here i am done with my code" );
+    }
+}
